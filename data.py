7e2620a44cd809d0d28db65d364837550ae4bad7
un-beep <2724836758@qq.com>
2024-12-08 16:10:16 +0800

    modified:   data.py

--- data.py
+++ data.py
@@ -55,59 +55,59 @@ Bearing_Fault_Data_URLs={
             "IR014_0": 'https://engineering.case.edu/sites/default/files/174.mat',
             "IR014_1": 'https://engineering.case.edu/sites/default/files/175.mat',
             "IR014_2": 'https://engineering.case.edu/sites/default/files/176.mat',
-            "IR014_3": 'https://engineering.case.edu/sites/default/files/177.mat',  #本文采用IR007_3数据
+            "IR014_3": 'https://engineering.case.edu/sites/default/files/177.mat',  #本文采用IR014_3数据
         },
 
         "Ball":{
             "B014_0": 'https://engineering.case.edu/sites/default/files/189.mat',
             "B014_1": 'https://engineering.case.edu/sites/default/files/190.mat',
             "B014_2": 'https://engineering.case.edu/sites/default/files/191.mat',
-            "B014_3": 'https://engineering.case.edu/sites/default/files/192.mat',  #本文采用B007_3数据
+            "B014_3": 'https://engineering.case.edu/sites/default/files/192.mat',  #本文采用B014_3数据
         },
 
         "Outer_Race_Centered":{
-                "OR014_6_0": 'https://engineering.case.edu/sites/default/files/109.mat',
-                "OR014_6_1": 'https://engineering.case.edu/sites/default/files/110.mat',
-                "OR014_6_2": 'https://engineering.case.edu/sites/default/files/111.mat',
-                "OR014_6_3": 'https://engineering.case.edu/sites/default/files/112.mat',  #本文采用IR007_3数据
+                "OR014_6_0": 'https://engineering.case.edu/sites/default/files/201.mat',
+                "OR014_6_1": 'https://engineering.case.edu/sites/default/files/202.mat',
+                "OR014_6_2": 'https://engineering.case.edu/sites/default/files/203.mat',
+                "OR014_6_3": 'https://engineering.case.edu/sites/default/files/204.mat',  #本文采用OR014_6_3数据
         },
 
     },
 
     "021inch":{
         "Inner_Race":{
-            "IR021_0": 'https://engineering.case.edu/sites/default/files/109.mat',
-            "IR021_1": 'https://engineering.case.edu/sites/default/files/110.mat',
-            "IR021_2": 'https://engineering.case.edu/sites/default/files/111.mat',
-            "IR021_3": 'https://engineering.case.edu/sites/default/files/112.mat',  #本文采用IR007_3数据
+            "IR021_0": 'https://engineering.case.edu/sites/default/files/213.mat',
+            "IR021_1": 'https://engineering.case.edu/sites/default/files/214.mat',
+            "IR021_2": 'https://engineering.case.edu/sites/default/files/215.mat',
+            "IR021_3": 'https://engineering.case.edu/sites/default/files/217.mat',  #本文采用IR021_3数据
         },
 
         "Ball":{
-            "B021_0": 'https://engineering.case.edu/sites/default/files/122.mat',
-            "B021_1": 'https://engineering.case.edu/sites/default/files/123.mat',
-            "B021_2": 'https://engineering.case.edu/sites/default/files/124.mat',
-            "B021_3": 'https://engineering.case.edu/sites/default/files/125.mat',  #本文采用B007_3数据
+            "B021_0": 'https://engineering.case.edu/sites/default/files/226.mat',
+            "B021_1": 'https://engineering.case.edu/sites/default/files/227.mat',
+            "B021_2": 'https://engineering.case.edu/sites/default/files/228.mat',
+            "B021_3": 'https://engineering.case.edu/sites/default/files/229.mat',  #本文采用B021_3数据
         },
 
         "Outer_Race_Centered":{
-                "OR021_6_0": 'https://engineering.case.edu/sites/default/files/109.mat',
-                "OR021_6_1": 'https://engineering.case.edu/sites/default/files/110.mat',
-                "OR021_6_2": 'https://engineering.case.edu/sites/default/files/111.mat',
-                "OR021_6_3": 'https://engineering.case.edu/sites/default/files/112.mat',  #本文采用IR007_3数据
+                "OR021_6_0": 'https://engineering.case.edu/sites/default/files/238.mat',
+                "OR021_6_1": 'https://engineering.case.edu/sites/default/files/239.mat',
+                "OR021_6_2": 'https://engineering.case.edu/sites/default/files/240.mat',
+                "OR021_6_3": 'https://engineering.case.edu/sites/default/files/241.mat',  #本文采用OR021_6_3数据
         },
 
         "Outer_Race_Orthogonal":{
-                "OR021_3_0": 'https://engineering.case.edu/sites/default/files/109.mat',
-                "OR021_3_1": 'https://engineering.case.edu/sites/default/files/110.mat',
-                "OR021_3_2": 'https://engineering.case.edu/sites/default/files/111.mat',
-                "OR021_3_3": 'https://engineering.case.edu/sites/default/files/112.mat',  #本文采用IR007_3数据
+                "OR021_3_0": 'https://engineering.case.edu/sites/default/files/250.mat',
+                "OR021_3_1": 'https://engineering.case.edu/sites/default/files/251.mat',
+                "OR021_3_2": 'https://engineering.case.edu/sites/default/files/252.mat',
+                "OR021_3_3": 'https://engineering.case.edu/sites/default/files/253.mat',  
         },
 
         "Outer_Race_Opposite":{
-                "OR021_12_0": 'https://engineering.case.edu/sites/default/files/109.mat',
-                "OR021_12_1": 'https://engineering.case.edu/sites/default/files/110.mat',
-                "OR021_12_2": 'https://engineering.case.edu/sites/default/files/111.mat',
-                "OR021_12_3": 'https://engineering.case.edu/sites/default/files/112.mat',  #本文采用IR007_3数据
+                "OR021_12_0": 'https://engineering.case.edu/sites/default/files/262.mat',
+                "OR021_12_1": 'https://engineering.case.edu/sites/default/files/263.mat',
+                "OR021_12_2": 'https://engineering.case.edu/sites/default/files/264.mat',
+                "OR021_12_3": 'https://engineering.case.edu/sites/default/files/265.mat',  
         },
 
     },
@@ -117,27 +117,50 @@ Bearing_Fault_Data_URLs={
 
 
 # 存储路径
-paths = { './data/Normal_Baseline_Data',
-          './data/48k_Drive_End_Bearing_Fault_Data/0.007inch',
-          './data/48k_Drive_End_Bearing_Fault_Data/0.014inch',
-          './data/48k_Drive_End_Bearing_Fault_Data/0.021inch',
+paths = { 
+            "Normal_Baseline_Data": './data/Normal_Baseline_Data',
+            "0.007inch": './data/48k_Drive_End_Bearing_Fault_Data/0.007inch',
+            "0.014inch": './data/48k_Drive_End_Bearing_Fault_Data/0.014inch',
+            "0.021inch": './data/48k_Drive_End_Bearing_Fault_Data/0.021inch',
 }
-
-# 确保目标文件夹存在
-for path in paths:
-    os.makedirs(path, exist_ok=True)
+    
 
 # 下载并保存文件
-for normal_url, url in normal_urls.items():
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # 如果请求失败，会抛出异常
-        path_name = os.path.join(path, f"{normal_url}.mat")
-        
-        # 以二进制模式写入文件
-        with open(path_name, 'wb') as f:
-            f.write(response.content)
-        print(f"文件 {normal_url}.mat 下载成功！")
-        
-    except requests.exceptions.RequestException as e:
-        print(f"下载 {normal_url} 时发生错误: {e}")
+for type_name, path in paths.items():
+    # 确保文件夹存在，不存在则创建文件夹
+    os.makedirs(path, exist_ok=True)
+
+    if path == './data/Normal_Baseline_Data':
+        for Normal_Baseline_Data_Name, url in Normal_Baseline_Data_URLs.items():
+            try:
+                response = requests.get(url)
+                response.raise_for_status()  # 如果请求失败，会抛出异常
+                path_name = os.path.join(path, f"{Normal_Baseline_Data_Name}.mat")
+
+                # 以二进制模式写入文件
+                with open(path_name, 'wb') as f:
+                    f.write(response.content)
+                print(f"文件 {Normal_Baseline_Data_Name}.mat 下载成功！")
+
+            except requests.exceptions.RequestException as e:
+                print(f"下载 {Normal_Baseline_Data_Name} 时发生错误: {e}")
+
+    for size_type, total_urls in Bearing_Fault_Data_URLs.items():
+        for fault_type, urls in total_urls.items():
+            # 根据故障位置分类保存到对应的文件夹内
+            path = path + f"/{size_type}/{fault_type}/" 
+            # 确保文件夹存在，不存在则创建文件夹
+            os.makedirs(path, exist_ok=True)
+            try:              
+                for data_name, url in urls.items():
+                    response = requests.get(url)
+                    response.raise_for_status()  # 如果请求失败，会抛出异常
+                    path_name = os.path.join(path, f"{data_name}.mat")
+
+                    # 以二进制模式写入文件
+                    with open(path_name, 'wb') as f:
+                        f.write(response.content)
+                    print(f"文件 {data_name}.mat 下载成功！")
+
+            except requests.exceptions.RequestException as e:
+                print(f"下载 {data_name} 时发生错误: {e}")
